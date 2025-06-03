7397b58ba9f1ebe8e418bebb4e2751a83afbdd52
farahfaisal <85195430+farahfaisal@users.noreply.github.com>
2025-06-03 04:57:18 +0300

    Update Store Name and Logo

---
+++ supabase/migrations/20250603015708_white_union.sql
@@ -0,0 +1,20 @@
+/*
+  # Update Store Settings
+  
+  1. Changes
+    - Update store name to "Nizar Cafe"
+    - Update store logo URL
+*/
+
+-- Update store settings with new values
+UPDATE settings
+SET value = jsonb_build_object(
+  'name', 'نزار كافيه',
+  'email', '',
+  'phone', '0597 672 268',
+  'address', 'عين يبرود -الشارع الرئيسي بحانب مدينة الطيبات, Ramallah, Palestine',
+  'logo', 'https://images.pexels.com/photos/1855214/pexels-photo-1855214.jpeg',
+  'vat', '',
+  'website', ''
+)
+WHERE key = 'store';
